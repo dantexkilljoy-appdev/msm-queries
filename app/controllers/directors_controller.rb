d4f8958cb4f824a8df0afc168a5df0ed6d595d6c
Dustin Doyle <doyledustin9@gmail.com>
2023-04-11 21:25:13 +0000

    working on bio to work

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,4 +5,33 @@ class DirectorsController < ApplicationController
 
     render({ template: "directors_templates/index.html.erb" })
   end
+
+  def wisest
+
+    @oldest = Director.where.not({ :dob => nil }).order({ dob: :asc }).at(0)
+
+    @oldest_name = @oldest.name
+
+    @oldest_dob = @oldest.dob
+
+    render({ template: "directors_templates/eldest.html.erb" })
+  end
+
+  def youngest
+
+    @youngest = Director.order({ dob: :desc }).at(0)
+
+    @youngest_name = @youngest.name
+
+    @youngest_dob = @youngest.dob
+
+    render({ template: "directors_templates/youngest.html.erb" })
+  end
+
+  def bio
+
+    @direct_id = params.fetch("direct_id")
+    
+    render({ template: "directors_templates/bio.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { controller: "directors", action: "index" })
+
+  get("/directors/eldest", { controller: "directors", action: "wisest" })
+  
+  get("/directors/youngest", { controller: "directors", action: "youngest" })
+
+  get("directors/:direct_id", { controller: "directors", action: "bio" })
 end
